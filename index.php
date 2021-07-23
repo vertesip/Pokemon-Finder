7b01f6b667c4983dfb225d057603a44ccc3fb7ed
vertesip <vertesip@gain.uni-sopron.hu>
2021-07-23 21:32:28 +0200

    Added random pokemon generator

--- index.php
+++ index.php
@@ -5,17 +5,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pokemon generator</title>
     <?php include "config.php" ?>
 </head>
 <body>
 <form method="post">
     <input type="text" placeholder="Pokemon name" name="pokemon">
     <button type="submit">Submit</button>
+    <button type="submit" name="random" value="<?php echo rand(1,300)?>">Random pokemon</button>
 </form>
 <?php
-$pokemon = file_get_contents("https://pokeapi.co/api/v2/pokemon/".$_POST["pokemon"] );
+if (isset($_POST["random"])){
+    $fetchedID = $_POST["random"];
+} else {
+    $fetchedID = $_POST["pokemon"];
+}
+$pokemon = file_get_contents("https://pokeapi.co/api/v2/pokemon/". $fetchedID);
 $pokemonDecoded = json_decode($pokemon, true); ?>
 <img src="<?php echo $pokemonDecoded['sprites']['other']['official-artwork']['front_default']; ?>" width="500px" height="500px">
+<img src="<?php echo $pokemonDecoded['sprites']['front_default']; ?>" width="300px" height="300px">
+
 </body>
 </html>
